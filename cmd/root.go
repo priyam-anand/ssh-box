4fab8af16f8d2c8196e48415111f5daffb4a72ba
Priyam Anand <priyam27.anand@gmail.com>
2024-08-25 01:05:48 +0530

    fix: replaced place holder detailed message

--- cmd/root.go
+++ cmd/root.go
@@ -12,13 +12,11 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ssh_box",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "SSH client and key manager",
+	Long: `ssh_box is a lightweight command-line tool for managing and 
+connecting to your servers via SSH. With ssh_box, you can easily 
+add, list, remove, and SSH into your registered servers using 
+simple commands.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
